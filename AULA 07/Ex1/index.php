665a36b112bbfb11b1a4d8f2fa5b3ff5296b759c
jggoncalez <joaogabriel.racer@gmail.com>
2025-10-20 16:56:24 -0300

    aula

---
+++ AULA 07/Ex1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert.php" method="POST">
+        <label>Nome:</label>
+        <input type="text" name="nome" required><br>
+        <label>Email:</label>
+        <input type="email" name="email" required><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
